f56eddd4abb016e6cdf4ce0812dcecbe664e2d58
alex <navy774989@gmail.com>
2022-07-07 09:07:27 +0800

    feat : add font

--- src/screens/entrance/HomeScreen/index.tsx
+++ src/screens/entrance/HomeScreen/index.tsx
@@ -1,6 +1,10 @@
-import { View } from "react-native"
+import { Text, View } from "react-native"
 import React from 'react'
 const HomeScreen = () => {
-    return <View style={{ flex: 1 }}></View>
+    return <View style={{ flex: 1, padding: 20, paddingTop: 40 }}>
+        <Text style={{ fontFamily: "NotoSansTC-Regular", fontSize: 18, }}>Hello</Text>
+        <Text style={{ fontFamily: "NotoSansTC-Bold", fontSize: 18 }}>Hello</Text>
+        <Text style={{ fontFamily: "NotoSansTC-Medium", fontSize: 18 }}>Hello</Text>
+    </View>
 }
 export default HomeScreen
